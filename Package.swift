0ecb8b5d7f5a1d457915645acc4b10f00fa8c4bf
Pete Schwamb <pete@schwamb.net>
2020-11-25 10:17:25 -0600

    Building via SPM

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ShareClient",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "ShareClient",
+            targets: ["ShareClient", "ShareClientUI"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/LoopKit/LoopKit.git", .branch("package-experiment"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "ShareClient",
+            dependencies: ["LoopKit"],
+            path: "ShareClient",
+            exclude: ["Info.plist"]
+        ),
+        .target(
+            name: "ShareClientUI",
+            dependencies: ["ShareClient", .product(name: "LoopKitUI", package: "LoopKit"), "LoopKit"],
+            path: "ShareClientUI",
+            exclude: ["Info.plist"]
+        ),
+        .testTarget(
+            name: "ShareClientTests",
+            dependencies: ["ShareClient"],
+            path: "ShareClientTests",
+            exclude: ["Info.plist"]
+        ),
+    ]
+)
