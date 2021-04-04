6c950abd9a09972f750edeb11b238812bf19a401
Pete Schwamb <pete@schwamb.net>
2021-04-04 12:32:33 -0700

    Run tests in example app

---
+++ ExampleApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ExampleApp
+//
+//  Created by Pete Schwamb on 4/4/21.
+//  Copyright © 2021 Mark Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Pete Schwamb on 4/4/21.
+//  Copyright © 2021 Mark Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
